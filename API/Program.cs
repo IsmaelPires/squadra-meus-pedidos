a63cb3edcc016078824f05cb2fa33eb8ecc53e2b
Ismael Pires <Ismael.Pires@fagrontech.com.br>
2025-03-05 18:31:17 -0300

    feat(logging): implementar Serilog para logging estruturado no PedidoService
    - Adicionado `ILogger<PedidoService>` via injeção de dependência
    - Implementados logs de início e sucesso nos métodos `ConsultarPedidoAsync` e `ListarPedidosPorStatusAsync`
    - Adicionados logs de erro detalhados para exceções capturadas
    - Melhorado o monitoramento de operações com logs estruturados
    
    Essa implementação melhora a rastreabilidade e depuração das operações relacionadas a pedidos.

--- API/Program.cs
+++ API/Program.cs
@@ -5,11 +5,22 @@ using Microsoft.EntityFrameworkCore;
 using Domain.Interfaces;
 using Data.Interfaces;
 using Data.Mapper;
+using Serilog;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+// Configuração do Serilog
+Log.Logger = new LoggerConfiguration()
+    .ReadFrom.Configuration(builder.Configuration) // Lê a configuração do appsettings.json
+    .Enrich.FromLogContext()
+    .WriteTo.Console() // Escreve logs no console
+    .WriteTo.File("logs/app.log", rollingInterval: RollingInterval.Day) // Escreve logs em arquivo
+    .CreateLogger();
 
+// Adicionar Serilog ao pipeline
+builder.Host.UseSerilog();
+
+// Add services to the container.
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
@@ -26,6 +37,8 @@ builder.Services.AddScoped(typeof(IRepository<,>), typeof(Repository<,>));
 builder.Services.AddAutoMapper(typeof(DataMappingProfile));
 
 var app = builder.Build();
+
+app.UseSerilogRequestLogging();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
