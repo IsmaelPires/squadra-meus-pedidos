bb30749b900b75924e0e82c146e4628768e0028e
Ismael Pires <Ismael.Pires@fagrontech.com.br>
2025-03-05 20:35:21 -0300

    feat(database): adiciona scripts de criação de tabelas e inserção de dados
    - Cria scripts para a tabela 'Cliente', 'Produto' 'Pedidos' e 'ItensPedido'.
    - Adiciona dados de exemplo para as tabelas recém-criadas

---
+++ Data/Database/Scripts/InsertData/InsertDataPedidos.sql
@@ -0,0 +1,12 @@
+-- Inserindo dados na tabela Pedidos
+INSERT INTO Pedidos (PedidoId, ClienteId, Imposto, Status) VALUES
+(1, 1, 5.00, 'Processando'),
+(2, 2, 10.00, 'Enviado'),
+(3, 3, 7.50, 'Entregue'),
+(4, 4, 8.00, 'Cancelado'),
+(5, 5, 9.25, 'Processando'),
+(6, 6, 4.75, 'Enviado'),
+(7, 7, 6.50, 'Entregue'),
+(8, 8, 11.00, 'Cancelado'),
+(9, 9, 5.50, 'Processando'),
+(10, 10, 12.00, 'Enviado');
