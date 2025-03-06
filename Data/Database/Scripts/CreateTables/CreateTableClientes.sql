0fa7fa289d4fefba835aa29af98c3920d4cb948a
Ismael Pires <Ismael.Pires@fagrontech.com.br>
2025-03-05 22:33:10 -0300

    chore: remove arquivos sqls desnecessários, pois já tem no readme.rm

--- Data/Database/Scripts/CreateTables/CreateTableClientes.sql
+++
@@ -1,6 +0,0 @@
-﻿-- Tabela Clientes
-CREATE TABLE Clientes (
-    ClienteId INT PRIMARY KEY,
-    Nome NVARCHAR(100) NOT NULL,
-    Email NVARCHAR(100) NOT NULL
-);
--- Data/Database/Scripts/CreateTables/CreateTablePedidos.sql
+++
@@ -1,9 +0,0 @@
--- Tabela Pedidos
-CREATE TABLE Pedidos (
-    PedidoId INT PRIMARY KEY,
-    ClienteId INT,
-    Imposto DECIMAL(10, 2),
-    Status NVARCHAR(50) NOT NULL,
-    DataPedido DATETIME DEFAULT GETDATE(),
-    CONSTRAINT FK_Pedidos_Clientes FOREIGN KEY (ClienteId) REFERENCES Clientes (ClienteId)
-);
